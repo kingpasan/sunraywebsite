9fb76ce435203151fc92eda7a1a034c16b1508d5
Pasan Maduranga Semage <pmsemage@gmail.com>
2023-02-15 17:32:57 +0100

    css fonts and primary added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
+        primary: "#e85c1b",
         secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        themeTransOrange: "#e85c1bd8",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        satisfy: ["Satisfy"]
       },
     },
     screens: {
